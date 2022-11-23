f165fa9204b741cd29a6a29dd131b75fbc43b9a1
Robert <rcrowley1107@gmail.com>
2022-11-23 14:13:34 -0500

    Section 3 end

---
+++ project/src/app/shopping-list/shopping-list.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { Ingredients } from '../shared/ingredients.model';
+@Component({
+  selector: 'app-shopping-list',
+  templateUrl: './shopping-list.component.html',
+  styleUrls: ['./shopping-list.component.css']
+})
+export class ShoppingListComponent {
+  ingredients: Ingredients[] = [
+    new Ingredients('apples', 5),
+    new Ingredients('tomatoes', 10),
+  ];
+  constructor() {}
+
+}
